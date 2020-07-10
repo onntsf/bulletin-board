5e13829c4c7faf74cb273219fe6c6f81e4d99375
onntsf <onntsf@gmail.com>
2020-07-10 11:45:11 +0900

    unfinished

--- ssr-server.js
+++ ssr-server.js
@@ -1,15 +1,15 @@
-const next = require("next");
-const express = require("express");
-const mysql = require("mysql");
+const next = require('next');
+const express = require('express');
+const mysql = require('mysql');
 
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const connection = mysql.createConnection({
-  host: "localhost",
-//  
-//  
-//  
+  host: 'localhost',
+//
+//
+//
 });
 
 app
@@ -17,20 +17,20 @@ app
   .then(() => {
     const server = express();
 
-    server.get("/", function(req, res) {
-      connection.query("SELECT * from t_board", function(err, rows, fields) {
+    server.get('/', (req, res) => {
+      connection.query('SELECT * from t_board', (err, rows, fields) => {
         if (err) throw err;
-//        res.send(rows[0])
-        app.render(req, res, '/index')
+        //        res.send(rows[0])
+        app.render(req, res, '/index');
       });
     });
 
-    server.listen(3000, err => {
+    server.listen(3000, (err) => {
       if (err) throw err;
-      console.log("> Ready on http://localhost:3000");
+      console.log('> Ready on http://localhost:3000');
     });
   })
-  .catch(ex => {
+  .catch((ex) => {
     console.error(ex.stack);
     process.exit(1);
   });
