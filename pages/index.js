ce91219152dc445e134748a91bd24f104fd503e1
onntsf <onntsf@gmail.com>
2020-07-10 11:37:10 +0900

    unfinished

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+const IndexPage = () => (
+  <>
+    <Head>
+      <title>Next.js • TodoMVC</title>
+    </Head>
+  hello world
+    <footer className="info">
+      <p>Double-click to edit a todo</p>
+      <p>Created by Misako Seki</p>
+    </footer>
+  </>
+);
+
+export default IndexPage;
---
+++ ssr-server.js
@@ -0,0 +1,36 @@
+const next = require("next");
+const express = require("express");
+const mysql = require("mysql");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const connection = mysql.createConnection({
+  host: "localhost",
+//  
+//  
+//  
+});
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.get("/", function(req, res) {
+      connection.query("SELECT * from t_board", function(err, rows, fields) {
+        if (err) throw err;
+//        res.send(rows[0])
+        app.render(req, res, '/index')
+      });
+    });
+
+    server.listen(3000, err => {
+      if (err) throw err;
+      console.log("> Ready on http://localhost:3000");
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
